ca8c5740bbec1c8a739608d91c0514a9f82c74a9
rishi kolvekar <rishi.kolvekar@accenture.com>
2016-05-03 15:22:36 +0530

    removed additional files

--- MyApplication2/app/src/main/java/com/startui/fragments/OnFragmentInteractionListener.java
+++
@@ -1,8 +0,0 @@
-package com.startui.fragments;
-
-import android.net.Uri;
-
-public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
